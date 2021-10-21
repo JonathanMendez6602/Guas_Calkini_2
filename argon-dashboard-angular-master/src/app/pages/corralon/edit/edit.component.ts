e4f581b1596930319075e5b5751bd8e15030c420
Irma Zi Canul <80069339+IrmaZi@users.noreply.github.com>
2021-10-21 10:05:30 -0500

    create

--- argon-dashboard-angular-master/src/app/pages/corralon/edit/edit.component.ts
+++ argon-dashboard-angular-master/src/app/pages/corralon/edit/edit.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CorralonService } from '../corralon.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { Corralon } from '../corralon';
+
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
@@ -7,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  corralon:Corralon;
+  form: FormGroup;
+
+  constructor(
+    public corralonService: CorralonService,
+    private route: ActivatedRoute,
+    private router: Router
+
+  ) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['idCorralon'];
+    this.corralonService.find(this.id).subscribe((data: Corralon)=>{
+      this.corralon = data;
+    })
+
+    this.form = new FormGroup({
+
+
+    });
+  }
+
+  get f(){
+    return this.form.controls;
+  }
+
+  submit(){
+    console.log(this.form.value);
+    this.corralonService.update(this.id, this.form.value).subscribe(res => {
+      console.log('Corralon update successfully');
+      this.router.navigateByUrl('corralon/index');
+    })
   }
 
 }
