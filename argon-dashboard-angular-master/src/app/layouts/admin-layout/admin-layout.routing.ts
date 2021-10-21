3640dda3bce388efdba75b931739a227a6b99590
Irma Zi Canul <80069339+IrmaZi@users.noreply.github.com>
2021-10-21 14:07:28 -0500

    index

--- argon-dashboard-angular-master/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ argon-dashboard-angular-master/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -6,18 +6,24 @@ import { MapsComponent } from '../../pages/maps/maps.component';
 
 import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
 import { TablesComponent } from '../../pages/tables/tables.component';
-import {IndexComponent} from '../../pages/vehiculo/index/index.component';
+
+import { IndexComponent } from '../../pages/vehiculo/index/index.component';
 import { CreateComponent } from '../../pages/vehiculo/create/create.component';
 import { EditComponent } from '../../pages/vehiculo/edit/edit.component';
 
-
+import { IndexCorralonComponent } from 'src/app/pages/corralon/index-corralon/index-corralon.component';
+import { CreateCorralonComponent } from 'src/app/pages/corralon/create-corralon/create-corralon.component';
+import { EditCorralonComponent } from 'src/app/pages/corralon/edit-corralon/edit-corralon.component';
 
 export const AdminLayoutRoutes: Routes = [
-    { path: 'dashboard',      component: DashboardComponent },
-    { path: 'user-profile',   component: UserProfileComponent },
-    { path: 'tables',         component: TablesComponent },
-    { path: 'vehiculo/index',          component: IndexComponent },
-    { path: 'maps',           component: MapsComponent },
-    { path: 'vehiculo/create',           component: CreateComponent },
-    { path: 'vehiculo/edit',           component: EditComponent }
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'user-profile', component: UserProfileComponent },
+    { path: 'tables', component: TablesComponent },
+    { path: 'vehiculo/index', component: IndexComponent },
+    { path: 'vehiculo/create', component: CreateComponent },
+    { path: 'vehiculo/edit', component: EditComponent },
+
+    { path: 'corralon/indexCorralon', component: IndexCorralonComponent },
+    { path: 'vehiculo/createCorralon', component: CreateCorralonComponent },
+    { path: 'vehiculo/editCorralon', component: EditCorralonComponent }
 ];
