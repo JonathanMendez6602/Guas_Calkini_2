3d7cde986ce6f3327e4b7be42b51ecf4f0b3b89a
Alejandro <you@example.com>
2021-10-26 13:20:47 -0500

    cambios

--- argon-dashboard-angular-master/src/app/components/sidebar/sidebar.component.ts
+++ argon-dashboard-angular-master/src/app/components/sidebar/sidebar.component.ts
@@ -11,7 +11,7 @@ export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Pagina Principal',  icon: 'ni-collection text-yellow', class: '' },
     { path: '/vehiculo/index', title: 'Servicios',  icon:'ni-delivery-fast text-yellow', class: '' },
     { path: '/corralon/indexCorralon', title: 'Corralon',  icon:'ni-ambulance text-yellow', class: '' },
-    { path: '/user-profile', title: 'Nuevo Servicio',  icon:'ni-bus-front-12 text-yellow', class: '' },
+    
     { path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-yellow', class: '' },
     { path: '/login', title: 'Login',  icon:'ni-key-25 text-yellow', class: '' },
     { path: '/register', title: 'Register',  icon:'ni-circle-08 text-yellow', class: '' }
