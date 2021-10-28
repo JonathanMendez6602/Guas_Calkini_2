c829dc1de6a6596a04b2da93bd62a7fac6c6c436
BrayanEscamilla <bescamilla481@gmail.com>
2021-10-28 00:09:10 -0500

    dash

--- argon-dashboard-angular-master/src/app/pages/dashboard/dashboard.component.ts
+++ argon-dashboard-angular-master/src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import Chart from 'chart.js';
 
-import { VehiculoService } from '../vehiculo.service';
-import { Vehiculo } from '../vehiculo';
-
 // core components
-
+import {
+  chartOptions,
+  parseOptions,
+  chartExample1,
+  chartExample2
+} from "../../variables/charts";
 
 @Component({
   selector: 'app-dashboard',
@@ -14,23 +16,45 @@ import { Vehiculo } from '../vehiculo';
 })
 export class DashboardComponent implements OnInit {
 
-  vehiculos: Vehiculo[] = [];
+  public datasets: any;
+  public data: any;
+  public salesChart;
+  public clicked: boolean = true;
+  public clicked1: boolean = false;
 
-  // constructor() { }
-  constructor(public VehiculoService: VehiculoService) { }
+  ngOnInit() {
 
-  ngOnInit(): void {
-    this.VehiculoService.getAll().subscribe((data: Vehiculo[])=>{
-      this.vehiculos = data;
-      console.log(this.vehiculos);
-    })
+    this.datasets = [
+      [0, 20, 10, 30, 15, 40, 20, 60, 60],
+      [0, 20, 5, 25, 10, 30, 15, 40, 40]
+    ];
+    this.data = this.datasets[0];
+
+
+    var chartOrders = document.getElementById('chart-orders');
+
+    parseOptions(Chart, chartOptions());
+
+
+    var ordersChart = new Chart(chartOrders, {
+      type: 'bar',
+      options: chartExample2.options,
+      data: chartExample2.data
+    });
+
+    var chartSales = document.getElementById('chart-sales');
+
+    this.salesChart = new Chart(chartSales, {
+			type: 'line',
+			options: chartExample1.options,
+			data: chartExample1.data
+		});
   }
 
-  deleteVehiculo(id){
-    this.VehiculoService.delete(id).subscribe(res => {
-         this.vehiculos = this.vehiculos.filter(item => item.id !== id);
-         console.log('Vehiculo deleted successfully!');
-    })
+
+  public updateOptions() {
+    this.salesChart.data.datasets[0].data = this.data;
+    this.salesChart.update();
   }
 
 }
