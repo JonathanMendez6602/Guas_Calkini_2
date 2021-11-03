7cb190854376bf4b1ff945c6e8f7c4160d2357e9
Alejandro <you@example.com>
2021-11-03 16:35:07 -0600

    create

--- argon-dashboard-angular-master/src/app/pages/chofer/create-chofer/create-chofer.component.ts
+++ argon-dashboard-angular-master/src/app/pages/chofer/create-chofer/create-chofer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ChoferService } from '../chofer.service';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-create-chofer',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateChoferComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    public choferService: ChoferService,
+    private router: Router,
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
+
+
+
+    this.form = new FormGroup({
+      nombre:  new FormControl('', [ Validators.required, Validators.pattern('^[a-zA-ZÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÑñüÜ \-\']+') ]),
+      apellido_p: new FormControl('', [ Validators.required, Validators.pattern ('^[a-zA-ZÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÑñüÜ0-9 \-\']+') ]),
+      apellido_m: new FormControl('', [ Validators.required, Validators.pattern ('^[a-zA-ZÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÑñüÜ0-9 \-\']+') ]),
+      edad: new FormControl('', [ Validators.required, Validators.pattern ("^[0-9]*$") ]),
+      doc_lic_fed:  new FormControl('', [ Validators.pattern('^[a-zA-ZÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÑñüÜ \-\']+') ]),
+      fecha_lic_fed: new FormControl(''),
+      num_lic_fed: new FormControl('', [ Validators.required, Validators.pattern ("^[0-9]*$") ]),
+      doc_lic_est:  new FormControl('', [ Validators.pattern('^[a-zA-ZÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÑñüÜ \-\']+') ]),
+      fecha_lic_est: new FormControl(''),
+      num_lic_est: new FormControl('', [ Validators.required, Validators.pattern ("^[0-9]*$") ]),
+      doc_ine: new FormControl('', [ Validators.required, Validators.pattern('^[a-zA-ZÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÑñüÜ \-\']+') ]),
+      doc_curp: new FormControl('', [ Validators.required, Validators.pattern('^[a-zA-ZÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÑñüÜ \-\']+') ]),
+    });
+
+  }
+
+  get f(){
+    return this.form.controls;
+  }
+
+  submit(){
+    console.log(this.form.value);
+    this.choferService.create(this.form.value).subscribe(res => {
+         console.log('Chofer created successfully!');
+         this.router.navigateByUrl('chofer/indexChofer');
+    })
   }
 
 }
