bcb9048bbbea0b71c3013e47dc940ad93fed392e
Alejandro <you@example.com>
2022-01-03 17:41:31 -0600

    Diseño

--- argon-dashboard-angular-master/src/app/template/components/sidebar/sidebar.component.ts
+++ argon-dashboard-angular-master/src/app/template/components/sidebar/sidebar.component.ts
@@ -8,15 +8,15 @@ declare interface RouteInfo {
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'ni-tv-2 text-primary', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
+    { path: '/dashboard', title: 'Dashboard',  icon: 'ni-collection text-yellow', class: '' },
+    /*{ path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
     { path: '/maps', title: 'Maps',  icon:'ni-pin-3 text-orange', class: '' },
     { path: '/user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '' },
-    { path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '' },
-    { path: '/vehiculo/index', title: 'Vehiculos',  icon:'ni-bullet-list-67 text-red', class: '' },
-    { path: '/corralon/indexCorralon', title: 'Corralones',  icon:'ni-bullet-list-67 text-red', class: '' },
-    { path: '/grua/indexGrua', title: 'Gruas',  icon:'ni-bullet-list-67 text-red', class: '' },
-    { path: '/chofer/indexChofer', title: 'Choferes',  icon:'ni-bullet-list-67 text-red', class: '' },
+    { path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '' },*/
+    { path: '/vehiculo/index', title: 'Servicios',  icon:'ni-delivery-fast text-yellow', class: '' },
+    { path: '/corralon/indexCorralon', title: 'Corralón',  icon:'ni-ambulance text-yellow', class: '' },
+    { path: '/grua/indexGrua', title: 'Grúas',  icon:'ni-bus-front-12 text-yellow', class: '' },
+    { path: '/chofer/indexChofer', title: 'Choferes',  icon:'ni-single-02 text-yellow', class: '' },
     { path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' },
     { path: '/register', title: 'Register',  icon:'ni-circle-08 text-pink', class: '' }
 ];
